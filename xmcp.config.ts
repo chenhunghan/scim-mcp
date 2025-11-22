31ff736fe0e081317cab97472efee63e2092e7bf
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-11-22 08:21:39 +0200

    fix: remove oauth
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- xmcp.config.ts
+++ xmcp.config.ts
@@ -7,19 +7,6 @@ const config: XmcpConfig = {
     prompts: false,
     resources: "./src/resources",
   },
-  experimental: {
-    oauth: {
-      baseUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com",
-      endpoints: {
-        authorizationUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com/authorize",
-        tokenUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com/oauth/token",
-        registerUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com/oidc/register",
-      },
-      issuerUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com",
-      defaultScopes: ["openid", "profile", "email"],
-      pathPrefix: "/oauth2",
-    },
-  },
 };
 
 export default config;
