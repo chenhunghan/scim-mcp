2be99acfdf2e834bab807adc7591eae1ab82d7b3
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-11-22 11:10:45 +0200

    fix: remove oauth, remove chatgpt ui
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- xmcp.config.ts
+++ xmcp.config.ts
@@ -6,20 +6,7 @@ const config: XmcpConfig = {
     tools: "./src/tools",
     prompts: false,
     resources: "./src/resources",
-  },
-  experimental: {
-    oauth: {
-      baseUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com",
-      endpoints: {
-        authorizationUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com/authorize",
-        tokenUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com/oauth/token",
-        registerUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com/oidc/register",
-      },
-      issuerUrl: "https://dev-k1hztrek6c61zbsx.eu.auth0.com",
-      defaultScopes: ["openid", "profile", "email"],
-      pathPrefix: "/oauth2",
-    },
-  },
+  }
 };
 
 export default config;
